2b84943eab3177df2b3fcbe87fc41a4a80806215
Nguyen Manh Loc <manhloc1st@gmail.com>
2023-10-06 08:06:12 +0700

    fix control

--- index.js
+++ index.js
@@ -12,15 +12,18 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
+const database = firebase.database();
 
 var nhietDo = document.getElementById("temp");
 var doAm = document.getElementById("humid");
 var khiGa = document.getElementById("gas");
 var luongMua = document.getElementById("rain");
-var dbRef = firebase.database().ref().child("Temp");
-var dbRef2 = firebase.database().ref().child("Humid");
-var dbRef3 = firebase.database().ref().child("Gas");
-var dbRef4 = firebase.database().ref().child("Rain");
+
+var dbRef = firebase.database().ref().child("/Sensor/Temp");
+var dbRef2 = firebase.database().ref().child("/Sensor/Humid");
+var dbRef3 = firebase.database().ref().child("/Sensor/Gas");
+var dbRef4 = firebase.database().ref().child("/Sensor/Rain");
+
 dbRef.on("value", (snap) => (nhietDo.innerText = snap.val()));
 dbRef2.on("value", (snap) => (doAm.innerText = snap.val()));
 dbRef3.on("value", (snap) => (khiGa.innerText = snap.val()));
@@ -30,42 +33,140 @@ dbRef4.on("value", (snap) => (luongMua.innerText = snap.val()));
 btnOffOne.onclick = function () {
     document.getElementById('btnOne').style.left = '0'
     document.getElementById('imgOne').src = "./assets/img/lightbulb-regular.svg"
+    database.ref("/Control").update({
+        Lamp: 0,
+    });
 }
 
 btnOnOne.onclick = function () {
     document.getElementById('btnOne').style.left = '43px'
     document.getElementById('imgOne').src = "./assets/img/lightbulb-solid.svg"
+    database.ref("/Control").update({
+        Lamp: 1,
+    });
 }
 
 btnOffTwo.onclick = function () {
     document.getElementById('btnTwo').style.left = '0'
     document.getElementById('imgTwo').src = "./assets/img/door-closed-solid.svg"
+    database.ref("/Control").update({
+        Door: 0,
+    });
 }
 
 btnOnTwo.onclick = function () {
     document.getElementById('btnTwo').style.left = '43px'
     document.getElementById('imgTwo').src = "./assets/img/door-open-solid.svg"
+    database.ref("/Control").update({
+        Door: 1,
+    });
 }
 
 btnOffThree.onclick = function () {
     document.getElementById('btnThree').style.left = '0'
     document.getElementById('imgThree').src = "./assets/img/fan-off.png"
+    database.ref("/Control").update({
+        Fan: 0,
+    });
 }
 
 btnOnThree.onclick = function () {
     document.getElementById('btnThree').style.left = '43px'
     document.getElementById('imgThree').src = "./assets/img/fan-on.png"
+    database.ref("/Control").update({
+        Fan: 1,
+    });
 }
 
 btnOffFour.onclick = function () {
     document.getElementById('btnFour').style.left = '0'
     document.getElementById('imgFour').src = "./assets/img/bell-off.png"
+    database.ref("/Control").update({
+        Buzzer: 0,
+    });
 }
 
 btnOnFour.onclick = function () {
     document.getElementById('btnFour').style.left = '43px'
     document.getElementById('imgFour').src = "./assets/img/bell-on.png"
+    database.ref("/Control").update({
+        Buzzer: 1,
+    });
 }
+
+// Tự động update hình ảnh
+database.ref("/Control/Lamp").on("value", function (snapshot) {
+    if (snapshot.exists()) {
+        var controlLamp = snapshot.val();
+        if (controlLamp == 1) {
+            document.getElementById('btnOne').style.left = '43px'
+            document.getElementById('imgOne').src = "./assets/img/lightbulb-solid.svg"
+        }
+        else {
+            document.getElementById('btnOne').style.left = '0'
+            document.getElementById('imgOne').src = "./assets/img/lightbulb-regular.svg"
+        }
+    } else console.log("No data available!");
+});
+
+database.ref("/Control/Door").on("value", function (snapshot) {
+    if (snapshot.exists()) {
+        var controlDoor = snapshot.val();
+        if (controlDoor == 1) {
+            document.getElementById('btnTwo').style.left = '43px'
+            document.getElementById('imgTwo').src = "./assets/img/door-open-solid.svg"
+        }
+        else {
+            document.getElementById('btnTwo').style.left = '0'
+            document.getElementById('imgTwo').src = "./assets/img/door-closed-solid.svg"
+        }
+    } else console.log("No data available!");
+});
+
+database.ref("/Control/Fan").on("value", function (snapshot) {
+    if (snapshot.exists()) {
+        var controlFan = snapshot.val();
+        if (controlFan == 1) {
+            document.getElementById('btnThree').style.left = '43px'
+            document.getElementById('imgThree').src = "./assets/img/fan-on.png"
+        }
+        else {
+            document.getElementById('btnThree').style.left = '0'
+            document.getElementById('imgThree').src = "./assets/img/fan-off.png"
+        }
+    } else console.log("No data available!");
+});
+
+database.ref("/Control/Buzzer").on("value", function (snapshot) {
+    if (snapshot.exists()) {
+        var controlBuzzer = snapshot.val();
+        if (controlBuzzer == 1) {
+            document.getElementById('btnFour').style.left = '43px'
+            document.getElementById('imgFour').src = "./assets/img/bell-on.png"
+        }
+        else {
+            document.getElementById('btnFour').style.left = '0'
+            document.getElementById('imgFour').src = "./assets/img/bell-off.png"
+        }
+    } else console.log("No data available!");
+});
+
+//get data once
+database
+    .ref("/Control")
+    .get()
+    .then((snapshot) => {
+        if (snapshot.exists()) console.log(snapshot.val());
+        else console.log("No data available!");
+    });
+
+database
+    .ref("/Sensor")
+    .get()
+    .then((snapshot) => {
+        if (snapshot.exists()) console.log(snapshot.val());
+        else console.log("No data available!");
+    });
 
 // Hiển thị thời gian
 function sokhongconghia(value) {
